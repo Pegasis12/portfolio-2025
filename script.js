37edea31e72a974df450b8feba55fb92a5c4703f
Pegasis12 <plsantana246@gmail.com>
2025-01-24 15:46:49 -0300

    final paginas

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 // animação color text
-// Espera a página carregar completamente
 document.addEventListener("DOMContentLoaded", () => {
-  // Seleciona o elemento com a classe 'destaque'
   const destaque = document.querySelector(".destaque");
 
-  // Remove a classe 'carregando' caso já tenha sido adicionada
   destaque.classList.remove("carregando");
 
-  // Reaplica a classe 'carregando' com um pequeno atraso
   setTimeout(() => {
     destaque.classList.add("carregando");
-  }, 50); // Pequeno atraso para reiniciar a animação
+  }, 50); // atraso para reiniciar a animação
 });

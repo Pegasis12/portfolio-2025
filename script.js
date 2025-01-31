07470401bf19a17f44991589970867454675184f
Pegasis12 <plsantana246@gmail.com>
2025-01-31 10:00:33 -0300

    menu-ativo

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ document.addEventListener("DOMContentLoaded", () => {
     destaque.classList.add("carregando");
   }, 50); // atraso para reiniciar a animação
 });
+
+//menu header para ativar o after do a ao ser clicado
+const links = document.querySelectorAll(".menu-informacoes a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  // metodo includes verifica se tem uma parte doq esta escrito no texto dentro de outro texto
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
